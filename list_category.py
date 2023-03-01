ad1cf57fa62be1b0ad4aeae1e76aa095945bbf2f
unknown <caucuachin@gmail.com>
2023-03-01 20:13:27 +0700

    update files

--- list_category.py
+++ list_category.py
@@ -9,10 +9,14 @@ products = db.products.find({}, {"productset_group_name": 1, "_id": 0})
 products = list(products)
 
 for product in products:
-    category = product['productset_group_name'].split('/')[-1]
-    if category in product_by_category:
-        product_by_category[category] += 1
-    else:
-        product_by_category[category] = 1
+    category_list = product['productset_group_name'].split('/')
+    for i in range(0,len(category_list)):
+        category = category_list[i]
+        if category in product_by_category:
+            product_by_category[category] += 1
+        else:
+            product_by_category[category] = 1
 
-# print(product_by_category)
+with open('count_category.txt', 'w', encoding='utf-8') as f:
+    for key, value in product_by_category.items():
+        f.write(key + ": " + str(value)+'\n')
